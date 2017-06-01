b76d464885a065c9ea55d8aa29636b9c952425d3
sofyanqusyairi <sofyanqusyairi@gmail.com>
2017-06-01 14:32:41 +0700

    init repo

---
+++ model.py
@@ -0,0 +1,6 @@
+class User():
+    def __init__(self, name):
+        self.__name = name
+
+    def getName(self):
+        return self.__name
---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, send_from_directory, request
+
+from model import User
+
+app = Flask(__name__)
+
+# configure static files
+@app.route('/<path:path>')
+def staticfile(path):
+    return send_from_directory('static', filename=path)
+
+# render index
+@app.route('/', methods=['GET'])
+def index():
+    title = "home"
+    name = "tom"
+    user = User(name)
+    return render_template('index.html', title = title, user = user)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug= True)
